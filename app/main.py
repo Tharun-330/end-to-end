ef6433c0ded6e67a5938ba69d255ebe25eccad82
Tharun-330 <tharunkumarthrn@gmail.com>
2025-11-02 21:47:17 +0530

    updated github actions file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DevOps CI/CD MultiEnv Pipeline!"
+    return "Hello from Tharun Kumar...!! Welcome to DevOps CI/CD MultiEnv Pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
